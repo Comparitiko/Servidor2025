583935f3029d1790b726d63939a85fc284b4421f
Comparitiko <gabigcy@gmail.com>
2024-09-26 18:01:48 +0200

    Preparacion para practica 1

--- src/index.php
+++ src/index.php
@@ -5,12 +5,42 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>NGINX y PHP</title>
-	<script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="bg-[#3f3f3f] text-[#ccc]">
+<style>
+	*,
+	*::before,
+	*::after {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	body {
+		background-color: #3f3f3f;
+		color: white;
+	}
+</style>
+
+<body>
+	<?=
+	"<h1>Generado con php</h1>";
+	?>
 	<?php
-	phpinfo();
+    if (isset($_GET['name'])) {
+	$nombre = $_GET['name'];
+	echo "<h2>Hola $nombre</h2>";
+    }
+    $precio = $_GET['price'];
+
+	echo !empty($precio) ? "<h2>El precio es: $precio</h2>" : "<h2>No hay precio</h2>";
+
+    $frutas = ['pera', 'manzana', 'limón'];
+
+    foreach ($frutas as $fruta) {
+	    echo "<h2>$fruta</h2>";
+    }
+
 	?>
 </body>
 
