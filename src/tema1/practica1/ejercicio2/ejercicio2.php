1ee8ce2bb6595d0a573eadb0a20144291dc84ece
Comparitiko <gabigcy@gmail.com>
2024-09-27 16:05:00 +0200

    Haciendo ejercicio2

--- src/tema1/practica1/ejercicio2/ejercicio2.php
+++ src/tema1/practica1/ejercicio2/ejercicio2.php
@@ -1,1 +1,179 @@
 <?php
+$imagesUrl = "http://localhost:8080/tema1/practica1/ejercicio2/images";
+$libreria = [
+  [
+    "ISBN" => "0000000000001",
+    "titulo" => "Todos los demonios del cielo",
+    "descripcion" => "Carmen ha cumplido el mandato de Dancaire y ha llegado hasta la Alhambra, donde descubrirá qué es lo que el rey del Infierno ha estado escondiendo con tanto recelo durante años.",
+    "categoria" => "novela",
+    "editorial" => "Faeris",
+    "foto" => "{$imagesUrl}/0000000000001.jpg",
+    "precio" => 18.52
+  ],
+  [
+    "ISBN" => "0000000000002",
+    "titulo" => "Todos los ángeles del infierno",
+    "descripcion" => "En un mundo oscuro en el que los demonios vencieron a los ángeles y en que los escasos supervivientes humanos viven aterrados y sometidos, los demonios campan a sus anchas por Andalucía para defender un oscuro secreto que Luzbel, el rey de los demonios, escondió en la Alhambra.",
+    "categoria" => "novela negra",
+    "editorial" => "Faeris",
+    "foto" => "{$imagesUrl}/0000000000002.jpg",
+    "precio" => 18.52
+  ],
+  [
+    "ISBN" => "0000000000003",
+    "titulo" => "Nenúfares negros",
+    "descripcion" => "Desde lo alto de su molino una anciana vigila el día a día del pueblo, los autobuses de los turistas siluetas y vidas que van pasando. Dos mujeres en particular destacan: una tiene los ojos del color de los nenúfares y sueña con el amor y la evasión; la otra, de once años, solo vive obsesionada para y por la pintura.",
+    "categoria" => "romántica, thriller",
+    "editorial" => "HarperCollins",
+    "foto" => "{$imagesUrl}/0000000000003.jpg",
+    "precio" => 5.65
+  ],
+  [
+    "ISBN" => "0000000000004",
+    "titulo" => "Una esposa de alquiler",
+    "descripcion" => "Siena Brabery, una vez una mujer de perfil discreto, experimentó una transformación impactante al pasar de la oscuridad al escrutinio público.",
+    "categoria" => "romántica",
+    "editorial" => "Publicación independiente",
+    "foto" => "{$imagesUrl}/0000000000004.jpg",
+    "precio" => 12.30
+  ],
+  [
+    "ISBN" => "0000000000005",
+    "titulo" => "Padre rico padre pobre",
+    "descripcion" => "Una nueva edición revisada y actualizada del bestseller que revolucionó la forma de entender las finanzas personales. Las premisas que Robert T. Kiyosaki estableció en esta obra han trascendido hasta hoy. Veinticinco años después de su lanzamiento, sigue siendo el libro de finanzas personales más vendido.",
+    "categoria" => "finanzas",
+    "editorial" => "Aguilar",
+    "foto" => "{$imagesUrl}/0000000000005.jpg",
+    "precio" => 18.90
+  ],
+  [
+    "ISBN" => "0000000000006",
+    "titulo" => "Mentalidad Topuria: todas las claves del éxito del campeón de la UFC",
+    "descripcion" => "",
+    "categoria" => "deporte",
+    "editorial" => "Roca editorial",
+    "foto" => "{$imagesUrl}/0000000000006.jpg",
+    "precio" => 18.52
+  ],
+  [
+    "ISBN" => "0000000000007",
+    "titulo" => "La paciente silenciosa",
+    "descripcion" => "A sus treinta y seis años, Mariana intenta recuperarse de la pérdida de Sebastian, el amor de su vida, ahogado durante unas vacaciones en una isla griega. Ella trabaja en Londres como terapeuta, pero cuando su sobrina Zoe, la única familia que le queda, la llama desde Cambridge para contarle que Tara, su mejor amiga, ha sido brutalmente asesinada cerca de la residencia de estudiantes, decide acudir en su ayuda.",
+    "categoria" => "novela negra",
+    "editorial" => "Alfaguara",
+    "foto" => "{$imagesUrl}/0000000000007.jpg",
+    "precio" => 20.80
+  ],
+  [
+    "ISBN" => "0000000000008",
+    "titulo" => "El juego del mal",
+    "descripcion" => "MADRID, 2018. El cadáver de una mujer vestida con ropas de muñeca aparece en la exclusiva urbanización de La Moraleja. Su rostro ha quedado irreconocible de un único disparo y sus huellas dactilares no constan en ninguna base de datos.",
+    "categoria" => "novela negra",
+    "editorial" => "Newton compton editores",
+    "foto" => "{$imagesUrl}/0000000000008.jpg",
+    "precio" => 12.25
+  ],
+  [
+    "ISBN" => "0000000000009",
+    "titulo" => "Los huérfanos",
+    "descripcion" => "Alguien arrastra por el bosque el cadáver de una chica. La han matado de una pedrada en la cabeza. El asesino la deja apoyada en unas rocas junto al río. Comienza a cavar un hoyo y la entierra junto a la figura de un San Pancracio.",
+    "categoria" => "novela negra",
+    "editorial" => "Publicación independiente",
+    "foto" => "{$imagesUrl}/0000000000009.jpg",
+    "precio" => 13.51
+  ],
+  [
+    "ISBN" => "0000000000010",
+    "titulo" => "La casa al final de la calle",
+    "descripcion" => "Una fría noche de invierno, Sharon Lemke sale al patio de su casa para ver el eclipse lunar. En la casa vecina, tras la ventana, distingue a una niña que está lavando los platos. ¿A quién se le ocurriría obligar a una criatura a hacer las tareas del hogar a esas horas de la noche? Una inquietante sensación la invade, y solo aumenta al recordar que la familia Fleming, en realidad, no tiene hijas pequeñas.",
+    "categoria" => "novela negra",
+    "editorial" => "Newton compton editores",
+    "foto" => "{$imagesUrl}/0000000000010.jpg",
+    "precio" => 12.25
+  ],
+  [
+    "ISBN" => "0000000000011",
+    "titulo" => "Luz de candelas",
+    "descripcion" => "Galán, seductor, ilustrado y amante de las letras, aventurero. Así es Luis Candelas, el joven que atrae las miradas de todo Madrid. De las mujeres, a quienes cautiva con gestos y palabras; de los hombres, por su simpatía y buen trato, pues quieren ser como él; y, por supuesto, de la policía y los grandes de España, que sólo quieren verlo preso. También el pequeño José Zorrilla cae ante sus encantos y, en cuanto lo conoce, su mundo entero salta por los aires.",
+    "categoria" => "novela historica",
+    "editorial" => "Editora y distribuidora Hispano americana S.A.",
+    "foto" => "{$imagesUrl}/0000000000011.jpg",
+    "precio" => 20.42
+  ],
+  [
+    "ISBN" => "0000000000012",
+    "titulo" => "La heredera del mar",
+    "descripcion" => "1348. La Corona de Aragón vive una de sus épocas más convulsas. Una enfermedad terrible y desconocida llega al puerto de Barcelona y comienza a propagarse por sus calles cuando Marina Montaner, descendiente de un largo linaje de mercaderes valencianos, desembarca en una Ciudad Condal sumida en el caos. Huyendo de los secuaces del rey Pedro IV el Ceremonioso, la joven es perseguida por una afrenta de la que ha sido injustamente acusada.",
+    "categoria" => "novela historica",
+    "editorial" => "Grijalbo",
+    "foto" => "{$imagesUrl}/0000000000012.jpg",
+    "precio" => 22.70
+  ],
+  [
+    "ISBN" => "0000000000013",
+    "titulo" => "Invencibles",
+    "descripcion" => "Durante decenios, desde Inglaterra se ha 'vendido' la derrota de nuestra Armada Invencible, en el Canal de la Mancha, como un gran éxito de los anglosajones. En realidad el combate quedó en tablas (los ingleses tuvieron más bajas), en el contexto de una guerra que duró 16 años y que, a la postre, ganó España.",
+    "categoria" => "novela historica",
+    "editorial" => "Libros en el bolsillo",
+    "foto" => "{$imagesUrl}/0000000000013.jpg",
+    "precio" => 9.45
+  ],
+  [
+    "ISBN" => "0000000000014",
+    "titulo" => "El sueño de los faraones",
+    "descripcion" => "El egiptólogo Émile Brugsch intenta averiguar la procedencia de algunos objetos valiosos que han aparecido en las tiendas de antigüedades de Luxor. Su instinto le dice que, detrás de esos objetos que se venden como recuerdos a los turistas, subyace una tupida red de traficantes que actúa sin el menor escrúpulo amparada por unas autoridades locales corruptas.",
+    "categoria" => "novela historica",
+    "editorial" => "Grijalbo",
+    "foto" => "{$imagesUrl}/0000000000014.jpg",
+    "precio" => 20.80
+  ],
+  [
+    "ISBN" => "0000000000015",
+    "titulo" => "La maldición de la reina Leonor",
+    "descripcion" => "Corre el año del Señor de 1184 y en una Castilla próspera y en paz reinan don Alfonso, el VIII de su nombre, y doña Leonor de Inglaterra. Sería el momento de disfrutar de todo lo alcanzado si no fuera porque la reina no consigue alumbrar al heredero varón que dé continuidad a la dinastía.",
+    "categoria" => "novela historica",
+    "editorial" => "Booket",
+    "foto" => "{$imagesUrl}/0000000000015.jpg",
+    "precio" => 9.45
+  ],
+]
+?>
+
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Ejercicio 2</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+  <div>
+    <header>
+      <h1>Librerìa Jaroso</h1>
+    </header>
+    <?php
+      function pintarLibro($libro) {
+        echo "<article>";
+        echo "<img src='{$libro["foto"]}' alt='Portada del libro {$libro["titulo"]}' />";
+        echo "<h2>{$libro["titulo"]}</h2>";
+        echo "<h3>{$libro["editorial"]}</h3>";
+        echo "<p>{$libro["precio"]}</p>";
+        echo "</article>";
+      }
+
+      function filterByCategory($libro, $category) {
+        return $libro["categoria"] === $category;
+      }
+
+      $novelasNegras = array_filter($libreria, function ($libro) {
+        return $libro["categoria"] === "novela negra";
+      });
+
+      foreach ($novelasNegras as $novelaNegra) {
+        pintarLibro($novelaNegra);
+      }
+    ?>
+  </div>
+</body>
+</html>
