77c44ecd3a9b266b70d4bd05fb910f0a3268e06e
Comparitiko <gabigcy@gmail.com>
2024-09-28 13:10:41 +0200

    Preparando ejercicio3 practica 1

---
+++ src/tema1/practica1/ejercicio3/README.md
@@ -0,0 +1,9 @@
+3. Web videojuego. Intenta hacer con diseño responsive (puedes usar Bootstrap)
+   una web que muestre unas 7 fichas de personajes (o clases) de un videojuego. Ten
+   en cuenta que toda la información mostrada debe salir de un array. Te recorres el
+   array y muestras el resultado. Decide la información a mostrar, puedes usar
+   alguna web de ejemplo e imitar el estilo.
+   Ten en cuenta también que cada elemento de un array puede ser a su vez un
+   array. Por ejemplo, para añadir varias características de un personaje, o varias
+   fotos.
+   Ejemplo: [Blizzard](https://us.diablo3.blizzard.com/es-mx/game/guide/getstarted/choosing-your-class)
--- src/tema1/practica1/ejercicio3/ejercicio3.php
+++ src/tema1/practica1/ejercicio3/ejercicio3.php
@@ -1,1 +1,170 @@
 <?php
+$imagesPath = "http://localhost:8080/tema1/practica1/ejercicio3/images";
+$pokemons = [
+  [
+    "name" => "Mewtwo",
+    "image" => "{$imagesPath}/mewtwo",
+    "types" => [
+      "first_type" => "psiquico",
+      "second_type" => "",
+    ],
+    "stats" => [
+      "hp" => 106,
+      "attack" => 110,
+      "defense" => 90,
+      "special-attack" => 154,
+      "special-defense" => 90,
+      "speed" => 130,
+    ],
+    "altura" => 20,
+    "peso" => 1220,
+  ],
+  [
+    "name" => "Raichu",
+    "image" => "{$imagesPath}/raichu",
+    "types" => [
+      "first_type" => "eléctrico",
+      "second_type" => "",
+    ],
+    "stats" => [
+      "hp" => 60,
+      "attack" => 90,
+      "defense" => 55,
+      "special-attack" => 90,
+      "special-defense" => 80,
+      "speed" => 110,
+    ],
+    "altura" => 8,
+    "peso" => 300,
+  ],
+  [
+    "name" => "Zamazenta",
+    "image" => "{$imagesPath}/zamazenta",
+    "types" => [
+      "first_type" => "lucha",
+      "second_type" => "",
+    ],
+    "stats" => [
+      "hp" => 92,
+      "attack" => 120,
+      "defense" => 115,
+      "special-attack" => 80,
+      "special-defense" => 115,
+      "speed" => 138,
+    ],
+    "altura" => 29,
+    "peso" => 2100,
+  ],
+  [
+    "name" => "Zacian",
+    "image" => "{$imagesPath}/zacian",
+    "types" => [
+      "first_type" => "hada",
+      "second_type" => "",
+    ],
+    "stats" => [
+      "hp" => 92,
+      "attack" => 120,
+      "defense" => 115,
+      "special-attack" => 115,
+      "special-defense" => 138,
+      "speed" => 130,
+    ],
+    "altura" => 28,
+    "peso" => 1100,
+  ],
+  [
+    "name" => "Pikachu",
+    "image" => "{$imagesPath}/pikachu",
+    "types" => [
+      "first_type" => "eléctrico",
+      "second_type" => "",
+    ],
+    "stats" => [
+      "hp" => 35,
+      "attack" => 55,
+      "defense" => 40,
+      "special-attack" => 50,
+      "special-defense" => 50,
+      "speed" => 90,
+    ],
+    "altura" => 4,
+    "peso" => 60,
+  ],
+  [
+    "name" => "Charizard",
+    "image" => "{$imagesPath}/charizard",
+    "types" => [
+      "first_type" => "fuego",
+      "second_type" => "volador",
+    ],
+    "stats" => [
+      "hp" => 78,
+      "attack" => 84,
+      "defense" => 78,
+      "special-attack" => 109,
+      "special-defense" => 85,
+      "speed" => 100,
+    ],
+    "altura" => 17,
+    "peso" => 905,
+  ],
+  [
+    "name" => "Bulbasaur",
+    "image" => "{$imagesPath}/bulbasaur",
+    "types" => [
+      "first_type" => "planta",
+      "second_type" => "",
+    ],
+    "stats" => [
+      "hp" => 45,
+      "attack" => 49,
+      "defense" => 49,
+      "special-attack" => 65,
+      "special-defense" => 65,
+      "speed" => 45,
+    ],
+    "altura" => 7,
+    "peso" => 69,
+  ],
+  [
+    "name" => "Squirtle",
+    "image" => "{$imagesPath}/squirtle",
+    "types" => [
+      "first_type" => "agua",
+      "second_type" => "",
+    ],
+    "stats" => [
+      "hp" => 44,
+      "attack" => 48,
+      "defense" => 65,
+      "special-attack" => 50,
+      "special-defense" => 64,
+      "speed" => 43,
+    ],
+    "altura" => 5,
+    "peso" => 90,
+  ]
+]
+?>
+
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Práctica 1 Ejercicio 3</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+  <header>
+
+  </header>
+
+  <main>
+
+  </main>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
