942b8b7f43a05add14acdfe202aec73b586782b9
Comparitiko <gabigcy@gmail.com>
2024-10-23 00:13:46 +0200

    Terminada practica 2 tema 2

--- src/tema2/practica2/index.php
+++ src/tema2/practica2/index.php
@@ -1,5 +1,9 @@
 <?php
   session_start();
+  if (!isset($_SESSION["partidas"])) $_SESSION["partidas"] = 0;
+  if (!isset($_SESSION["victorias"])) $_SESSION["victorias"] = 0;
+  if (!isset($_SESSION["derrotas"])) $_SESSION["derrotas"] = 0;
+
 ?>
 <!doctype html>
 <html lang="es">
@@ -16,23 +20,26 @@
   </style>
 </head>
 <body>
-  <header>
-    <h1 class="bg-danger text-center text-white">Juego de las siete y media</h1>
+  <header class="bg-danger p-3">
+    <h1 class="text-center text-white">Juego de las siete y media</h1>
   </header>
   <main>
     <div class="container my-4">
       <?php
       if (isset($_GET["jugador"])) {
+        $_SESSION["partidas"] += 1;
         if (strcmp($_GET["jugador"], "ganador") == 0) {
+          $_SESSION["victorias"] += 1;
           echo "
             <div class='alert alert-success' role='alert'>
-              <h2 class='text-success'>Ganaste wee</h2>  
+              <h2 class='text-success'>Ganaste la partida</h2>  
             </div>
           ";
         } else if (strcmp($_GET["jugador"], "perdedor") == 0) {
+          $_SESSION["derrotas"] += 1;
           echo "
             <div class='alert alert-danger' role='alert'>
-              <h2 class='text-danger'>Perdiste wee</h2>  
+              <h2 class='text-danger'>Perdiste la partida</h2>  
             </div>
           ";
         }
@@ -65,9 +72,17 @@
       </div>
       <?php
       // Comprobar si hay cartas sacadas, si no hay cartas pone el boton de reset, si no hay nada no lo pone
-      if (isset($_SESSION["cartas-sacadas"]) && (count($_SESSION["cartas-sacadas"]) != 0))
-        echo "<a href='controlador.php?accion=reset' class='btn btn-primary'>Reset</a>"
+      if (isset($_SESSION["cartas-sacadas"]) && (count($_SESSION["cartas-sacadas"]) != 0)) {
+        echo "<a href='controlador.php?accion=reset' class='btn btn-primary'>Reset</a>";
+      }
       ?>
+      <footer class="mt-5">
+        <ul>
+          <li>Partidas jugadas: <span class="fs-5"><?= $_SESSION["partidas"]; ?></span></li>
+          <li>Partidas ganadas: <span class="fs-5"><?= $_SESSION["victorias"]; ?></span></li>
+          <li>Partidas perdidas: <span class="fs-5"><?= $_SESSION["derrotas"]; ?></span></li>
+        </ul>
+      </footer>
     </div>
   </main>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
