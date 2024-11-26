fa9c071a798ed6cc489745fb180df54822cc8ab9
Comparitiko <gabigcy@gmail.com>
2024-11-26 16:53:54 +0100

    Modificado index.php

--- src/index.php
+++ src/index.php
@@ -9,7 +9,10 @@
 </head>
 
 <body class="bg-slate-900 text-white flex flex-col gap-4 p-4 justify-content-center align-items-center w-100 h-100">
-  <a class="m-auto hover-shadow-lg" href="./tema5/practica1/index.php">Practica coworking con MongoDB</a>
+  <a class="m-auto hover:text-red-500" href="./tema4/index.php">Practica coworking con MariaDB</a>
+  <a class="m-auto hover:text-red-500" href="./tema4_p2/index.php">Practica BlackJack</a>
+  <a class="m-auto hover:text-red-500" href="./tema5/practica1/index.php">Practica coworking con MongoDB</a>
+  <a class="m-auto hover:text-red-500" href="./tema5/practica2/index.php">Practica Dragon Ball API</a>
+  <a class="m-auto hover:text-red-500" href="./tema5/practica3/index.php">Practica Blog con AI</a>
 </body>
-
 </html>
