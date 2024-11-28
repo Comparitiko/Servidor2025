5f90a0b30c4af777a826c320f75c9f826fd2e2cb
Comparitiko <gabigcy@gmail.com>
2024-11-28 11:54:47 +0100

    Preparando proyecto BlogsAI

--- entrypoints/php.entrypoint.sh
+++ entrypoints/php.entrypoint.sh
@@ -2,15 +2,17 @@
 
 # Entrar al directorio de la practica1 tema 5
 cd /var/www/html/tema5/practica1
-
+rm -rf vendor
+rm -rf composer.lock
 # Instalar dependencias
 composer install
 
-## Entrar al directorio de la practica3 tema 5
-#cd /var/www/html/tema5/practica3
-#
-## Instalar dependencias
-#composer install
+# Entrar al directorio de la practica3 tema 5
+cd /var/www/html/tema5/practica3
+rm -rf vendor
+rm -rf composer.lock
+# Instalar dependencias
+composer install
 
 # Ejecutar PHP-FPM como proceso principal
 exec php-fpm
