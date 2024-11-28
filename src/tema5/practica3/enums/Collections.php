58d120218e581e487c4aaf7d2288c9086e754ab1
Comparitiko <gabigcy@gmail.com>
2024-11-28 12:07:06 +0100

    Preparando proyecto BlogsAI

---
+++ src/tema5/practica3/enums/Collections.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ChatGPTBlogs\enums;
+
+enum Collections: string
+{
+  case USERS = 'users';
+
+  case BLOGS = 'blogs';
+}
--- src/tema5/practica3/models/DBConnection.php
+++ src/tema5/practica3/models/DBConnection.php
@@ -2,7 +2,49 @@
 
 namespace ChatGPTBlogs\models;
 
+use ChatGPTBlogs\enums\Collections;
+use MongoDB\Client;
+use MongoDB\Collection;
+use MongoDB\Exception\Exception;
+
+require "./vendor/autoload.php";
+
 class DBConnection
 {
+  private Client|null $client;
 
+  /**
+   * Connect to database
+   * @return void
+   */
+  public function __construct()
+  {
+    $mongoUri = 'mongodb://gabriel:gabriel123@mongodb:27017'; // Uri to connect to mongo database
+    try {
+      $this->client = new Client($mongoUri);
+    } catch (Exception $e) {
+      $this->client = null;
+    }
+  }
+
+  /**
+   * Get the collection that is passed from parameter
+   * @param Collections $collection
+   * @return Collection|null
+   */
+  public function getCollection(Collections $collection): Collection|null
+  {
+    if (is_null($this->client)) return null;
+
+    return $this->client->selectCollection('coworking', $collection->value);
+  }
+
+  /**
+   * Close connection of the database
+   * @return void
+   */
+  public function closeConnection(): void
+  {
+    $this->client = null;
+  }
 }
